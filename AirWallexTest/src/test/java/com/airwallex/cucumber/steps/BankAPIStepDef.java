baf94441c303ac94f4fc7c6f05e6eaef224fbc97
Chacko, Mily <Mily.Chacko@tabcorp.com.au>
2020-02-20 11:03:26 +1100

    AirWallex code commit

--- AirWallexTest/src/test/java/com/airwallex/cucumber/steps/BankAPIStepDef.java
+++ AirWallexTest/src/test/java/com/airwallex/cucumber/steps/BankAPIStepDef.java
@@ -1,35 +1,14 @@
 package com.airwallex.cucumber.steps;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.openqa.selenium.WebDriver;
-
-
 import com.airwallex.cucumber.serenity.BankAPISteps;
-import com.airwallex.pojo.Bankdetails;
-import com.airwallex.utils.ReusableSpecifications;
 import com.airwallex.utils.TestDataBuild;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
-import io.restassured.specification.ResponseSpecification;
-import net.bytebuddy.utility.privilege.SetAccessibleAction;
-import net.serenitybdd.junit.runners.SerenityParameterizedRunner;
-import net.serenitybdd.rest.SerenityRest;
-import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Steps;
-import net.thucydides.junit.annotations.UseTestDataFrom;
 
-//@RunWith(SerenityParameterizedRunner.class)
-//@UseTestDataFrom("testdata/ATOTaxCalTestData.csv")
 
 public class BankAPIStepDef {
 
@@ -37,14 +16,11 @@ public class BankAPIStepDef {
 
 	TestDataBuild data = new TestDataBuild();
 
-	// @Managed(driver="chrome", uniqueSession = true)
-	// WebDriver driver;
 
 	@Steps
 	BankAPISteps bankdetails;
 
-	//@Steps
-	//AccNumberSteps accNumber;
+
 
 	@Given("^Add Bank Details Payload with payment method\"([^\"]*)\" country code\"([^\"]*)\" acc name\"([^\"]*)\" acc number\"([^\"]*)\" swift code\"([^\"]*)\" bsb\"([^\"]*)\" aba\"([^\"]*)\"$")
 	public void add_Bank_Details_Payload(String payment_method, String bank_country_code, String account_name,
